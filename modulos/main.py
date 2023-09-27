359f086012da498938d38dd2afc3d2673275fe40
fbenav <francisco.blorca@gmail.com>
2023-09-26 22:05:23 -0300

    corregimos la ruta de lectura de los archivos

--- modulos/main.py
+++ modulos/main.py
@@ -1,18 +1,22 @@
 import pandas as pd
+from procesamiento import procesamiento
+from seleccion_modelos import modelos
 
-df = pd.read_csv('/datasets/users_behavior.csv')
+
+df = pd.read_csv('C:/Users/franc/Documents/Repositorios/TT_DS_proyecto_7/data/users_behavior.csv')
 
 
-list_data_procesada = procesamiento(df)
+lista_data_procesada = procesamiento(df)
 
 features_train = lista_data_procesada[0] 
 features_test  = lista_data_procesada[1] 
 features_valid = lista_data_procesada[2] 
 target_train = lista_data_procesada[3] 
 target_test = lista_data_procesada[4] 
-target_valid =lista_data_procesada[5] 
+target_valid =lista_data_procesada[5]
 
-best_model = modelamiento(train)
 
-df_predicciones = prediccioens(best_model, test)
+best_model = modelos(features_train, target_train, features_valid, target_valid)
+
+#df_predicciones = prediccioens(best_model, test)
 
